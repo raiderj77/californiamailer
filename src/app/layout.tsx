80bcedc7ece30ec1918bbf6977e37956a229ce4a
Jason Ramirez <raiderj77@gmail.com>
2026-02-01 12:14:25 -0800

    Fix duplicate body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>{children}</AuthProvider>
         <GoogleAnalytics gaId="G-E2KDN733NL" />
-     </body>
+       </body>
      </html>
   );
 }
